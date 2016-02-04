05355cb1d27aac81d61f968c811280013ffb5393
Amalia Viti <amalia.viti@gmail.com>
2016-02-03 23:02:50 -0500

    switches to using Promises

--- test/mta.js
+++ test/mta.js
@@ -7,7 +7,7 @@ describe('MTA', function () {
     serviceType,
     stopId,
     stopIds;
-  
+
   before(function (done) {
     mta = new Mta({
       key: config.key
@@ -17,79 +17,72 @@ describe('MTA', function () {
     serviceType = 'subway';
     done();
   });
-  
-  it('should return info for all MTA subway stops', function (done) {
-    mta.stop(function (err, result) {
-      should.not.exist(err);
+
+  it('should return info for all MTA subway stops', function () {
+    return mta.stop()
+    .then(function (result) {
       result.should.be.an.Object;
-      done();
     });
   });
-  
-  it('should get info for 1 MTA subway stop', function (done) {    
-    mta.stop(stopId, function (err, result) {
-      should.not.exist(err);
+
+  it('should get info for 1 MTA subway stop', function () {
+    return mta.stop(stopId)
+    .then(function (result) {
       result.stop_id.should.equal(stopId.toString());
       result.should.have.property('stop_name');
-      done();
     });
   });
-  
-  it('should get info for multiple MTA subway stop', function (done) {
-    mta.stop(stopIds, function (err, result) {
-      should.not.exist(err);
+
+  it('should get info for multiple MTA subway stop', function () {
+    return mta.stop(stopIds)
+    .then(function (result) {
       stopIds.forEach(function (val) {
         result.should.have.property(val);
       });
-      done();
     });
   });
-  
-  it('should get MTA service status for all types', function (done) {
-    mta.status(function (err, result) {
-      should.not.exist(err);
+
+  it('should get MTA service status for all types', function () {
+    return mta.status()
+    .then(function (result) {
       result.should.have.property('subway');
       result.should.have.property('bus');
       result.should.have.property('BT');
       result.should.have.property('LIRR');
       result.should.have.property('MetroNorth');
-      done();
     });
   });
-  
-  it('should get MTA service status for 1 type', function (done) {
-    mta.status(serviceType, function (err, result) {
-      should.not.exist(err);
+
+  it('should get MTA service status for 1 type', function () {
+    return mta.status(serviceType)
+    .then(function (result) {
       result.should.be.an.Array;
-      done();
     });
   });
-  
+
   it('api key is set in test/config.js', function (done) {
     config.key.should.not.equal('your-api-key');
     done();
   });
-  
-  it('should get schedule info for 1 MTA subway station', function (done) {
-    mta.schedule(stopId, function(err, result) {
-      should.not.exist(err);
+
+  it('should get schedule info for 1 MTA subway station', function () {
+    return mta.schedule(stopId)
+    .then(function (result) {
       result.should.have.property('schedule');
       result.should.have.property('updatedOn');
       result.schedule[stopId].should.exist;
-      done();
     });
   });
-  
-  it('should get schedule info for multiple MTA subway stations', function (done) {
-    mta.schedule(stopIds, function (err, result) {
-      should.not.exist(err);
+
+  it('should get schedule info for multiple MTA subway stations', function () {
+    return mta.schedule(stopIds)
+    .then(function (result) {
       result.should.have.property('schedule');
       result.should.have.property('updatedOn');
       stopIds.forEach(function (val) {
         result.schedule.should.have.property(val);
       });
-      done();
     });
   });
-    
+
 });
